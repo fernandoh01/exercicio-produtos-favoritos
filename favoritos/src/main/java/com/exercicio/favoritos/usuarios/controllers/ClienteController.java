d1c4b5645bdf081b2534413651195a4dfaa702f9
Fernando Simaro <fernando.simaro@koiketec.com>
2024-01-29 22:40:42 -0300

    correcoes dominio usuario

--- favoritos/src/main/java/com/exercicio/favoritos/usuarios/controllers/ClienteController.java
+++ favoritos/src/main/java/com/exercicio/favoritos/usuarios/controllers/ClienteController.java
@@ -1,10 +1,96 @@
 package com.exercicio.favoritos.usuarios.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.exercicio.favoritos.usuarios.dto.ClienteDto;
+import com.exercicio.favoritos.usuarios.entidades.Usuario;
+import com.exercicio.favoritos.usuarios.repositorios.UsuarioRepositorio;
+
 @RestController
-@RequestMapping("/clientes")
 public class ClienteController {
-    
+
+    private final UsuarioRepositorio usuarioRepositorio;
+
+    ClienteController(UsuarioRepositorio rep){
+        this.usuarioRepositorio = rep;
+    }
+
+    @GetMapping("/clientes")
+    ResponseEntity<List<Usuario>> recuperaTodosClientes(){
+        try {
+            List<Usuario> res =  this.usuarioRepositorio.findAll();
+            if(res == null || res.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }else{
+                return ResponseEntity.ok(res);
+            }
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+        
+    }
+
+    @GetMapping("/clientes/{cliente_id}")
+    ResponseEntity<Usuario> recuperaClientePeloId(@PathVariable Long cliente_id){
+        try {
+            Optional<Usuario> res = this.usuarioRepositorio.findById(cliente_id);
+            if (res.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }else{
+                return ResponseEntity.ok(res.get());
+            }
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PostMapping("/clientes")
+    ResponseEntity<Usuario> novoCliente(@RequestBody ClienteDto novoCliente){
+        try {
+            Optional<Usuario> jaCadastrado = this.usuarioRepositorio.findByEmail(novoCliente.getEmail());
+            if(jaCadastrado.isPresent()){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);    
+            }
+            Usuario add = new Usuario();
+            add.setNome(novoCliente.getNome());
+            add.setEmail(novoCliente.getEmail());
+            Usuario usuarioCriado =  this.usuarioRepositorio.save(add);
+            return ResponseEntity.ok(usuarioCriado);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PatchMapping("/clientes/{cliente_id}")
+    ResponseEntity<Usuario> atualizaDadosClientes(@PathVariable Long cliente_id, @RequestBody ClienteDto clienteAtualizado){
+        try {
+            Optional<Usuario> jaCadastrado = this.usuarioRepositorio.findByEmail(clienteAtualizado.getEmail());
+            if(jaCadastrado.isPresent()){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);    
+            }
+            Optional<Usuario> res = this.usuarioRepositorio.findById(cliente_id);
+            if (res.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }else{
+                Usuario u = res.get();
+                u.setNome(clienteAtualizado.getNome());
+                u.setEmail(clienteAtualizado.getEmail());
+                this.usuarioRepositorio.save(u);
+                return ResponseEntity.ok(res.get());
+            }
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
--- favoritos/src/main/java/com/exercicio/favoritos/usuarios/entidades/Usuario.java
+++ favoritos/src/main/java/com/exercicio/favoritos/usuarios/entidades/Usuario.java
@@ -2,6 +2,8 @@ package com.exercicio.favoritos.usuarios.entidades;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -17,6 +19,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Table(name = "usuarios")
 @Entity(name = "Usuario")
@@ -24,6 +27,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 @Getter
+@Setter
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,8 +36,12 @@ public class Usuario {
     @Column(unique = true)
     private String email;
 
+    private String nome;
+
+    @JsonIgnore
     private String senha;
 
+    @JsonIgnore
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
     @JoinTable(name="usuarios_acessos",
             joinColumns = @JoinColumn(name = "usuarios_id"),
